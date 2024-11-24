770fb7cf223d1f7550a14569df2f95da4a12de2e
Amirwsen <A.h.aghazadeh.1381@Gmail.com>
2024-11-25 01:04:42 +0330

    Add Domain Layer

--- Domain/Requests/TaskUpdateRequest.cs
+++ Domain/Requests/TaskUpdateRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Domain.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Requests;
 
 public class TaskUpdateRequest
 {
+    [MaxLength(25)]
     public string? Title { get; set; }
     public string? Description { get; set; }
 }
